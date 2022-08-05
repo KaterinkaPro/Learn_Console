2d907dd293448294a54a9636048cbf17b3a67645
KaterinkaPro <pronina.nw@mail.ru>
2022-08-05 22:01:19 +0300

    add hw2

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -4,25 +4,25 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3*/
 
-int a = Convert. ToInt32(Console.ReadLine());
-int b = Convert. ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 int max = a;
 {
-    if(a<b)
+    if (a < b)
     {
         Console.Write("Большее число - ");
         Console.WriteLine(b);
-         Console.Write("Меньшее число - ");
+        Console.Write("Меньшее число - ");
         Console.Write(a);
-       
+
     }
-else
-    if(a>b)
+    else
+    if (a > b)
     {
         Console.Write("Большее число - ");
         Console.WriteLine(a);
         Console.Write("Меньшее число - ");
         Console.Write(b);
-       
+
     }
 }
