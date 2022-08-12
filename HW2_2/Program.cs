c5e01b9da4cfce6adc33f6200fdaa0ccf45d1911
KaterinkaPro <pronina.nw@mail.ru>
2022-08-13 00:30:26 +0300

    add second task

---
+++ HW2_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+Console.Write($"Ожидается ввод числа: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a = Math.Abs(num);
+string stringNum = Convert.ToString(a);
+
+if (stringNum.Length < 3)
+{
+    Console.WriteLine("Введенное число меньше трехзначного");
+}
+else
+{
+    Console.WriteLine("Третья цифра: " + stringNum[2]);
+}
