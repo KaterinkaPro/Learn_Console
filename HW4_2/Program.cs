7e7fc743a04f23a9ad3f4d66d11f33d91fcce5fd
KaterinkaPro <pronina.nw@mail.ru>
2022-08-17 23:47:10 +0300

    add task 4.2

---
+++ HW4_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+class program
+{
+    static int Numb(int value)
+    {
+        if (value<10)
+        return value;
+        int digit = value %10;
+        int nextValue = value / 10;
+
+        return digit + Numb(nextValue);
+    }
+        static void Main(string[] args)
+        {
+           Console.Write($"Give me your number! - ");
+            int MyValue = Convert.ToInt32(Console.ReadLine());
+            int result = Numb(MyValue);
+            Console.WriteLine ($"Сумма введенных цифр: - {result}");
+        }
+}
