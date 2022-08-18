73130b973a4140d8789a7070482a9d72fcc820d2
KaterinkaPro <pronina.nw@mail.ru>
2022-08-18 10:55:18 +0300

    add task 4.3

---
+++ HW4_3/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+class program
+{
+    static void WriteArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        int[] array = new int[0];
+        Console.Write($"Ожидается ввод чисел: \nпо окончанию введите end:");
+        while (true)
+        {
+
+            string meaning = Console.ReadLine();
+            if (meaning != "end")
+            {
+                int number = Convert.ToInt32(meaning);
+                int[] NewArray = array;
+                array = new int[array.Length + 1];
+                for (int i = 0; i < NewArray.Length; i++)
+                {
+                    array[i] = NewArray[i];
+                }
+                array[array.Length - 1] = number;
+            }
+
+            else
+            {
+                break;
+            }
+
+        }
+        WriteArray(array);
+    }
+
+
+}
+
