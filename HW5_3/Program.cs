f045043b1b14b9c6f7c3aefbd000459f1a3f2fab
KaterinkaPro <pronina.nw@mail.ru>
2022-08-27 12:30:47 +0300

    add task 5.3

---
+++ HW5_3/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76*/
+
+
+
+using System;
+
+namespace homework
+{
+    class Program
+    {
+        static int MaxNumArray(int[] array)
+        {
+            int max = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+        static int MinNumArray(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 12, 10, 2, 78, 26, 124, 3, 8, 90, 45 };
+            int max = MaxNumArray(array);
+            int min = MinNumArray(array);
+            Console.WriteLine(max - min);
+
+        }
+    }
+}
